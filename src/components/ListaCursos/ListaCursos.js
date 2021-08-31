4bb6eaf9bab5d32630c71228f21d1d20b80923dc
TiagoLobo77 <tiago_batista77@outlook.com>
2021-08-31 13:23:06 -0300

    aula-01-05

---
+++ src/components/ListaCursos/ListaCursos.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { ApiService } from "../../data/services/ApiService";
+
+function ListaCursos() {
+  const [cursos, setCursos] = useState([]);
+
+  console.log(cursos);
+
+  function buscarCursos() {
+    ApiService.get("cursos").then((data) => setCursos(data));
+  }
+
+  return (
+    <div>
+      <button onClick={buscarCursos}>Procurar Cursos</button>
+      <ul>
+        {cursos.map((item) => (
+          <li key={item.id}>{item.nome}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ListaCursos;
